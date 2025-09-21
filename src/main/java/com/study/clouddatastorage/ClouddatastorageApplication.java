5d802fcd9a75a08c691f7d2886efc12e9c620b04
Grisha <dinsuil275283@gmail.com>
2025-09-21 08:19:26 +0500

    Initial commit

---
+++ src/main/java/com/study/clouddatastorage/ClouddatastorageApplication.java
@@ -0,0 +1,13 @@
+package com.study.clouddatastorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClouddatastorageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClouddatastorageApplication.class, args);
+    }
+
+}
